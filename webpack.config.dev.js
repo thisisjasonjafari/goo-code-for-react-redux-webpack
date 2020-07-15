d4deb7ca55e1589955ad18e24d68b08b0cbad027
Jason <jason.jafari.m@gmail.com>
2020-07-14 21:37:53 -0400

    make react app run suscessfull

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,12 +2,12 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-process.env.NODE_ENV = "developmet";
+process.env.NODE_ENV = "development";
 
 module.exports = {
     mode: "development",
     target: "web",
-    devTool: "cheap-module-source-map",
+    devtool: "cheap-module-source-map",
     entry: "./src/index",
     output: {
         path: path.resolve(__dirname, "build"),
@@ -32,7 +32,7 @@ module.exports = {
         rules: [{
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"],
+                use: ["babel-loader", "eslint-loader"],
             },
             {
                 test: /(\.css)$/,
