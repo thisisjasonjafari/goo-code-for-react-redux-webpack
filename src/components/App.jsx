a9b3377e117abb6d2e268b1113479761a746d99d
Jason <jason.jafari.m@gmail.com>
2020-07-14 23:46:44 -0400

    use react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from 'react-router-dom'
 import HomePage from "./home/Home.jsx";
 import AboutPage from "./about/About.jsx";
+import Header from "./common/Header.jsx";
 
 const App = () => {
     return (
         <div className="container-fluid">
+            <Header />
             <Route
                 exact path="/"
                 component={HomePage}
