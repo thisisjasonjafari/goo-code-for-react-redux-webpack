651e7d1fb54494a5e54ce75d4cda15b8a716e857
Jason <jason.jafari.m@gmail.com>
2020-07-16 09:22:10 -0400

    some work with state

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 
 class CoursesPage extends Component {
+
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value }
+    this.setState({ course: course })
+    // console.log(event.target.value)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state.course.title)
+  }
   render() {
-    return <h2>Courses</h2>
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+        <input type="submit" name="" id="" value="Save" />
+      </form>
+    )
   }
 }
 
